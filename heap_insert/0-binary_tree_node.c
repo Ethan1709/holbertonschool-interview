776a2231a142e6fde61602958655784e6af2a376
Ethan1709 <benyayerethan@gmail.com>
2024-01-15 13:23:30 +0100

    add comments

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -1,6 +1,14 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - creates a binary tree
+ * @parent: the parent ptr
+ * @value: value of each node
+ *
+ * Return: the new node
+ */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
