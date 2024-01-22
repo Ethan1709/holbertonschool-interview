16222e2f423b01a5c8f9b8a8343ab8bf848e8c4b
Ethan1709 <benyayerethan@gmail.com>
2024-01-22 01:30:58 +0100

    add readme

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -6,17 +6,15 @@ operations needed to result in exactly n H characters in the file.
 
 
 def minOperations(n):
+    div, nb_operations = 2, 0
+
     if n <= 1:
         return 0
-    
-    div = 2
-    ope = 0
-
     while(n > 1):
         if (n % div == 0):
             n //= div
-            ope += div
+            nb_operations += div
         else:
             div += 1
 
-    return ope
+    return nb_operations
