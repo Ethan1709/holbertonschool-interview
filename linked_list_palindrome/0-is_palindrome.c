86420d4e93674d732edffe5456098410840a5245
Ethan1709 <benyayerethan@gmail.com>
2024-02-05 23:20:51 +0100

    fix malloc

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -18,7 +18,7 @@ int is_palindrome(listint_t **head)
     if (head == NULL)
         return 1;
 
-    listint_t* temp = *head;
+    listint_t *temp = *head;
     while (temp)
     {
         temp = temp->next;
