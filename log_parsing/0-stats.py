dc314b8e213c5e86cace955a24edf6ce5001116c
Ethan1709 <benyayerethan@gmail.com>
2024-02-12 18:59:35 +0100

    increase optimisation

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -10,48 +10,41 @@ status_available = ['200', '301', '400', '401', '403', '404', '405', '500']
 total_size = 0
 
 
+def print_stats(total_size, my_dict):
+    print("File size: {}".format(total_size))
+    for key in sorted(my_dict):
+        print("{}: {}".format(key, my_dict[key]))
+
+
+
 try:
     for line in sys.stdin:
         line_split = line.split()
 
         try:
-            status_code = line_split[7]
-        except IndexError:
+            file_size = line_split[8]
+            total_size += int(file_size)
+        except (IndexError, ValueError):
             pass
         
         try:
-            file_size = line_split[8]
+            status_code = line_split[7]
             if status_code in status_available:
                 if status_code in my_dict:
-                    my_dict[status_code].extend([file_size])
+                    my_dict[status_code] += 1
                 else:
-                    my_dict[status_code] = [file_size]
-            else:
-                break
-        except IndexError:
+                    my_dict[status_code] = 1
+        except (IndexError, ValueError):
             pass
 
-        total_size += int(file_size)
         i += 1
-        if i == 10:
-            print('File size: {}'.format(total_size))
-            my_dict = sorted(my_dict.items())
-            my_dict = dict(my_dict)
-            for key in my_dict:
-                print('{}: {}'.format(key, len(my_dict[key])))   
-            i = 0
 
-    print('File size: {}'.format(total_size))
-    my_dict = sorted(my_dict.items())
-    my_dict = dict(my_dict)
-    for key in my_dict:
-        print('{}: {}'.format(key, len(my_dict[key])))
-        i = 0
+        if i == 10:
+            print_stats(total_size, my_dict)
+            i = 0
+    
+    print_stats(total_size, my_dict)
 
 except KeyboardInterrupt:
-    print('File size: {}'.format(total_size))
-    my_dict = sorted(my_dict.items())
-    my_dict = dict(my_dict)
-    for key in my_dict:
-        print('{}: {}'.format(key, len(my_dict[key])))
+    print_stats(total_size, my_dict)
     raise
