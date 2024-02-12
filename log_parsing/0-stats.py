9f37aab02e2f38b23ad8a938f3cf54812e37233f
Ethan1709 <benyayerethan@gmail.com>
2024-02-12 18:14:48 +0100

    change logic total size

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -10,31 +10,39 @@ status_available = ['200', '301', '400', '401', '403', '404', '405', '500']
 total_size = 0
 
 
-for line in stdin:
-    line_split = line.split()
-    status_code = line_split[7]
-    file_size = line_split[8]
-    if status_code in status_available:
-        if status_code in my_dict:
-            my_dict[status_code].extend([file_size])
+try:
+    for line in stdin:
+        line_split = line.split()
+        status_code = line_split[7]
+        file_size = line_split[8]
+        if status_code in status_available:
+            if status_code in my_dict:
+                my_dict[status_code].extend([file_size])
+            else:
+                my_dict[status_code] = [file_size]
         else:
-            my_dict[status_code] = [file_size]
-    else:
-        break
-    total_size += int(file_size)
-    i += 1
-    if i == 10:
-        print(f'File size: {total_size}')
-        my_dict = sorted(my_dict.items())
-        my_dict = dict(my_dict)
-        for key in my_dict:
-            print(f'{key}: {len(my_dict[key])}')
-        my_dict = {}
+            break
+        total_size += int(file_size)
+        i += 1
+        if i == 10:
+            print(f'File size: {total_size}')
+            my_dict = sorted(my_dict.items())
+            my_dict = dict(my_dict)
+            for key in my_dict:
+                print(f'{key}: {len(my_dict[key])}')   
+            i = 0
+
+    print(f'File size: {total_size}')
+    my_dict = sorted(my_dict.items())
+    my_dict = dict(my_dict)
+    for key in my_dict:
+        print(f'{key}: {len(my_dict[key])}')
         i = 0
 
-if i != 0:
+except KeyboardInterrupt:
     print(f'File size: {total_size}')
-my_dict = sorted(my_dict.items())
-my_dict = dict(my_dict)
-for key in my_dict:
-    print(f'{key}: {len(my_dict[key])}')
+    my_dict = sorted(my_dict.items())
+    my_dict = dict(my_dict)
+    for key in my_dict:
+        print(f'{key}: {len(my_dict[key])}')
+    raise
