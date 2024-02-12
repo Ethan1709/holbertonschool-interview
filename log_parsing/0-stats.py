4911c0f265239e13db0f1856ab7defa71796f147
Ethan1709 <benyayerethan@gmail.com>
2024-02-12 17:43:44 +0100

    check pep8

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -5,6 +5,7 @@ from sys import stdin
 
 i = 0
 my_dict = {}
+status_available = ['200', '301', '400', '401', '403', '404', '405', '500']
 total_size = 0
 
 
@@ -12,10 +13,13 @@ for line in stdin:
     line_split = line.split()
     status_code = line_split[7]
     file_size = line_split[8]
-    if status_code in my_dict:
-        my_dict[status_code].extend([file_size])
+    if status_code in status_available:
+        if status_code in my_dict:
+            my_dict[status_code].extend([file_size])
+        else:
+            my_dict[status_code] = [file_size]
     else:
-        my_dict[status_code] = [file_size]
+        break
     total_size += int(file_size)
     i += 1
     if i == 10:
@@ -27,9 +31,9 @@ for line in stdin:
         my_dict = {}
         i = 0
 
-print(f'File size: {total_size}')
+if i != 0:
+    print(f'File size: {total_size}')
 my_dict = sorted(my_dict.items())
 my_dict = dict(my_dict)
 for key in my_dict:
     print(f'{key}: {len(my_dict[key])}')
-   
